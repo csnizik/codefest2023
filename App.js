d01179a41a92157c3f0639ffffc9cff7fd33e471
Christian Acosta <Christian.Acosta.Developer@gmail.com>
2023-07-21 10:54:26 -0400

    add start button

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useState } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGameStart = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Button
+        title="Press to Start"
+        loading={isLoading}
+        loadingIndicatorPosition="overlay"
+        onPress={handleGameStart}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
